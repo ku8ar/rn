9f720ce94ea886aa4c471f618d5136eb98768473
Kuba Juszczyk <juszczyk.jakub@gmail.com>
2025-04-04 18:04:58 +0200

    works

--- ios/build-bridge.sh
+++ ios/build-bridge.sh
@@ -11,7 +11,7 @@ XCWORKSPACE="rnbridge.xcworkspace"
 REACT_NATIVE_CLI="../node_modules/react-native/cli.js"
 
 # Clean previous builds
-rm -rf "$BUILD_DIR"
+# rm -rf "$BUILD_DIR"
 rm -rf "$OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
 
@@ -41,24 +41,24 @@ xcodebuild archive \
   BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 echo "📦 Building $SCHEME for simulator (iphonesimulator)..."
-xcodebuild archive \
-  -workspace "$XCWORKSPACE" \
-  -scheme "$SCHEME" \
-  -configuration "$CONFIGURATION" \
-  -sdk iphonesimulator \
-  -destination "generic/platform=iOS Simulator" \
-  -archivePath "$BUILD_DIR/ios_simulator.xcarchive" \
-  SKIP_INSTALL=NO \
-  BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+# xcodebuild archive \
+#   -workspace "$XCWORKSPACE" \
+#   -scheme "$SCHEME" \
+#   -configuration "$CONFIGURATION" \
+#   -sdk iphonesimulator \
+#   -destination "generic/platform=iOS Simulator" \
+#   -archivePath "$BUILD_DIR/ios_simulator.xcarchive" \
+#   SKIP_INSTALL=NO \
+#   BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 # Manually copy JS bundle to device framework
 cp Bridge/main.jsbundle "$BUILD_DIR/ios_devices.xcarchive/Products/Library/Frameworks/$FRAMEWORK_NAME"
-cp Bridge/main.jsbundle "$BUILD_DIR/ios_simulator.xcarchive/Products/Library/Frameworks/$FRAMEWORK_NAME"
+# cp Bridge/main.jsbundle "$BUILD_DIR/ios_simulator.xcarchive/Products/Library/Frameworks/$FRAMEWORK_NAME"
 
 echo "🧱 Creating XCFramework..."
 xcodebuild -create-xcframework \
   -framework "$BUILD_DIR/ios_devices.xcarchive/Products/Library/Frameworks/$FRAMEWORK_NAME" \
-  -framework "$BUILD_DIR/ios_simulator.xcarchive/Products/Library/Frameworks/$FRAMEWORK_NAME" \
+  # -framework "$BUILD_DIR/ios_simulator.xcarchive/Products/Library/Frameworks/$FRAMEWORK_NAME" \
   -output "$OUTPUT_DIR/Bridge.xcframework"
 
 echo "✅ Done! XCFramework is in $OUTPUT_DIR/Bridge.xcframework"
